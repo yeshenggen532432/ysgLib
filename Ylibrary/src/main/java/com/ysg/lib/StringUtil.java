c9e8c0aa3bd14dd2cf94b2ca79d9925b9d87ea7f
叶生根 <1617683532@qq.com>
2023-04-24 05:57:12 +0800

    yy

---
+++ Ylibrary/src/main/java/com/ysg/lib/StringUtil.java
@@ -0,0 +1,7 @@
+package com.ysg.lib;
+
+public class StringUtil {
+    public static boolean isEmpty(String s){
+        return true;
+    }
+}
--- app/src/main/java/com/android/myapplication/MainActivity.java
+++ app/src/main/java/com/android/myapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ysg.lib.YToast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
